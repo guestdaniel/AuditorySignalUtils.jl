d2d937dd3c34eef9e012946f68783be035c74f21
Daniel Guest <guestusut@gmail.com>
2021-10-20 17:09:19 -0500

    Adjusted type annotations in function signatures
    Generally, type annotations for scalar parameters were dropped (so that
    users can freely pass ints / floats and let the downstream stage
    handle those details) and type annotations for 1d sound vectors were
    generalized to abstract floats (so users can easily adjust precision).

--- src/AuditorySignalUtils.jl
+++ src/AuditorySignalUtils.jl
@@ -8,7 +8,7 @@ using Statistics
 
 Amplifies a signal's power by a certain amount in dB (or attenuates the signal if dB is negative)
 """
-function amplify(signal::Array{Float64, 1}, dB::Float64)::Array{Float64, 1}
+function amplify(signal::Array{<:AbstractFloat, 1}, dB)::Array{<:AbstractFloat, 1}
     signal .* 10^(dB/20)
 end
 
@@ -19,7 +19,7 @@ end
 Applies a raised-cosine ramp to the input signal, where dur_ramp is the duration of each
 samp segment in seconds
 """
-function cosine_ramp(signal::Array{Float64, 1}, dur_ramp::Float64, fs::Float64)::Array{Float64, 1}
+function cosine_ramp(signal::Array{<:AbstractFloat, 1}, dur_ramp, fs)::Array{<:AbstractFloat, 1}
     t = LinRange(0, 0.25, Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
     ramp_segment = sin.(2*pi*t).^2
     ramp = [ramp_segment; ones(length(signal) - length(ramp_segment)*2); reverse(ramp_segment)]
@@ -28,11 +28,11 @@ end
 
 
 """
-    dbspl(signal)::Float64
+    dbspl(signal)::AbstractFloat
 
 Calculates the dB SPL value of a signal (assuming that the units of the signal are in Pascals)
 """
-function dbspl(signal::Array{Float64, 1})::Float64
+function dbspl(signal::Array{<:AbstractFloat, 1})::AbstractFloat
     20*log10(rms(signal)/20e-6)
 end
 
@@ -44,26 +44,23 @@ function LogRange(a, b, n::Int)
     10 .^ LinRange(log10(a), log10(b), n)
 end
 
+
 """
     pure_tone(freq, phase, dur, fs)
 
 Synthesizes a pure tone with specified frequency, phase offset (in radians), duration, and sampling rate.
 """
-function pure_tone(freq::Float64, phase::Float64, dur::Float64, fs::Float64)::Array{Float64, 1}
-    sin.(2*pi*freq*LinRange(0, dur, Int64(dur*fs)) .+ phase)
-end
-
-function pure_tone(freq::Float64, phase::Irrational, dur::Float64, fs::Float64)::Array{Float64, 1}
+function pure_tone(freq, phase, dur, fs)::Array{<:AbstractFloat, 1}
     sin.(2*pi*freq*LinRange(0, dur, Int64(dur*fs)) .+ phase)
 end
 
 
 """
-    rms(signal)::Float64
+    rms(signal)::AbstractFloat
 
 Calculates the room-mean-square (RMS) of a signal
 """
-function rms(signal::Array{Float64, 1})::Float64
+function rms(signal::Array{<:AbstractFloat, 1})::AbstractFloat
     sqrt(mean(signal.^2))
 end
 
@@ -73,7 +70,7 @@ end
 
 Adjusts a signals level to be a certain level in dB SPL
 """
-function scale_dbspl(signal::Array{Float64, 1}, dB::Float64)::Array{Float64, 1}
+function scale_dbspl(signal::Array{<:AbstractFloat, 1}, dB)::Array{<:AbstractFloat, 1}
     curr_dB = dbspl(signal)
     delta_dB = dB - curr_dB
     amplify(signal, delta_dB)
