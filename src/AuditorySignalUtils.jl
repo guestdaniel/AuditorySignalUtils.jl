2c2811f74522f882b0fd9b170052af9366639104
Daniel Guest <guestusut@gmail.com>
2021-11-09 11:29:10 -0600

    Added in-place versions of many functions and fixed doc strings
    Added in-place versions of amplify!, cosine_ramp!, and scale_dbspl! which
    result in less allocation than the standard copy-based versions. Also
    fixed msot doc strings to allow proper specialization over numeric types.

--- src/AuditorySignalUtils.jl
+++ src/AuditorySignalUtils.jl
@@ -1,5 +1,5 @@
 module AuditorySignalUtils
-export amplify, cosine_ramp, dbspl, LogRange, pure_tone, rms, scale_dbspl 
+export amplify, amplify!, cosine_ramp, cosine_ramp!, dbspl, LogRange, pure_tone, rms, scale_dbspl, scale_dbspl!
 
 using Statistics
 
@@ -8,8 +8,19 @@ using Statistics
 
 Amplifies a signal's power by a certain amount in dB (or attenuates the signal if dB is negative)
 """
-function amplify(signal::Array{<:AbstractFloat, 1}, dB)::Array{<:AbstractFloat, 1}
+function amplify(signal::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:Real}
     signal .* 10^(dB/20)
+end
+
+
+"""
+    amplify!(signal, dB)
+
+Amplifies a signal's power by a certain amount in dB (or attenuates the signal if dB is negative)
+"""
+function amplify!(signal::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:Real}
+    signal .*= 10^(dB/20)
+    return signal
 end
 
 
@@ -19,7 +30,7 @@ end
 Applies a raised-cosine ramp to the input signal, where dur_ramp is the duration of each
 samp segment in seconds
 """
-function cosine_ramp(signal::Array{<:AbstractFloat, 1}, dur_ramp, fs)::Array{<:AbstractFloat, 1}
+function cosine_ramp(signal::AbstractVector{T}, dur_ramp::T, fs::T)::AbstractVector{T} where {T<:Real}
     t = LinRange(0, 0.25, Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
     ramp_segment = sin.(2*pi*t).^2
     ramp = [ramp_segment; ones(length(signal) - length(ramp_segment)*2); reverse(ramp_segment)]
@@ -28,11 +39,26 @@ end
 
 
 """
+    cosine_ramp!(signal, dur_ramp, fs)
+
+Applies a raised-cosine ramp to the input signal, where dur_ramp is the duration of each
+samp segment in seconds
+"""
+function cosine_ramp!(signal::AbstractVector{T}, dur_ramp::T, fs::T)::AbstractVector{T} where {T<:Real}
+    t = LinRange(0, 0.25, Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
+    ramp_segment = sin.(2*pi*t).^2
+    ramp = [ramp_segment; ones(length(signal) - length(ramp_segment)*2); reverse(ramp_segment)]
+    signal .*= ramp
+    return signal
+end
+
+
+"""
     dbspl(signal)::AbstractFloat
 
-Calculates the dB SPL value of a signal (assuming that the units of the signal are in Pascals)
+Calculates the dB SPL value of a signal (assuming that the units of the signal are in Pa)
 """
-function dbspl(signal::Array{<:AbstractFloat, 1})::AbstractFloat
+function dbspl(signal::AbstractVector{T})::T where {T<:Real}
     20*log10(rms(signal)/20e-6)
 end
 
@@ -40,7 +66,7 @@ end
 """
     LogRange(a, b, n)::Range
 """
-function LogRange(a, b, n::Int)
+function LogRange(a::T, b::T, n::Int) where {T<:Real}
     10 .^ LinRange(log10(a), log10(b), n)
 end
 
@@ -50,8 +76,8 @@ end
 
 Synthesizes a pure tone with specified frequency, phase offset (in radians), duration, and sampling rate.
 """
-function pure_tone(freq, phase, dur, fs)::Array{<:AbstractFloat, 1}
-    sin.(2*pi*freq*LinRange(0, dur, Int64(dur*fs)) .+ phase)
+function pure_tone(freq::T, phase::T, dur::T, fs::T)::AbstractVector{T} where {T<:Real}
+    sin.(2*pi .* freq .* (0.0:(1/fs):prevfloat(dur)) .+ phase)
 end
 
 
@@ -60,7 +86,7 @@ end
 
 Calculates the room-mean-square (RMS) of a signal
 """
-function rms(signal::Array{<:AbstractFloat, 1})::AbstractFloat
+function rms(signal::AbstractVector{T})::T where {T<:Real}
     sqrt(mean(signal.^2))
 end
 
@@ -70,10 +96,20 @@ end
 
 Adjusts a signals level to be a certain level in dB SPL
 """
-function scale_dbspl(signal::Array{<:AbstractFloat, 1}, dB)::Array{<:AbstractFloat, 1}
+function scale_dbspl(signal::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:Real}
     curr_dB = dbspl(signal)
     delta_dB = dB - curr_dB
     amplify(signal, delta_dB)
 end
+
+"""
+    scale_dbspl!(signal, dB)
+
+Adjusts a signals level to be a certain level in dB SPL
+"""
+function scale_dbspl!(signal::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:Real}
+    amplify!(signal, dB - dbspl(signal))
+end
+
 
 end # module
