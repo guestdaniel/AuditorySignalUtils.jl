f632310632d32b9438f68c57c028c43bbcfbd19d
Daniel Guest <guestusut@gmail.com>
2021-11-15 15:40:12 -0600

    Fixed cosine_ramp and removed 1.7-specific code
    A previous commit had included 1.7-specific use of escape characters
    in the docstrings, which I then realized wouldn't compile on 1.6. I'll
    have to leave those changes for a 1.7 branch that can be merged once
    the standard release switches to 1.7.

--- src/AuditorySignalUtils.jl
+++ src/AuditorySignalUtils.jl
@@ -8,8 +8,8 @@ export amplify, amplify!, cosine_ramp, cosine_ramp!, dbspl, LogRange, pure_tone,
 """
     amplify(signal, dB)
 
-Amplifies a signal (in terms of power) by an amount in dB (or attenuates the signal if dB\
- is negative)
+Amplifies a signal (in terms of power) by an amount in dB (or attenuates the signal if dB 
+is negative)
 """
 function amplify(signal::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:Real}
     signal .* 10^(dB/20)
@@ -19,9 +19,11 @@ end
 """
     amplify!(signal, dB)
 
-Amplifies a signal (in terms of power) by an amount in dB (or attenuates the signal if dB is negative).
+Amplifies a signal (in terms of power) by an amount in dB (or attenuates the signal if dB 
+is negative).
 
-Note, this version of the function operates in-place (i.e., no extra memory is allocated and the input is modified in-place)
+Note, this version of the function operates in-place (i.e., no extra memory is allocated 
+and the input is modified in-place)
 """
 function amplify!(signal::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:Real}
     signal .*= 10^(dB/20)
@@ -32,11 +34,11 @@ end
 """
     cosine_ramp(signal, dur_ramp, fs)
 
-Applies a raised-cosine ramp to the input signal, where dur_ramp is the duration of each
+Applies a raised-cosine ramp to the input signal, where dur_ramp is the duration of each 
 samp segment in seconds
 """
 function cosine_ramp(signal::AbstractVector{T}, dur_ramp::T, fs::T)::AbstractVector{T} where {T<:Real}
-    t = LinRange(0, 0.25, Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
+    t = LinRange(0, prevfloat(0.25), Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
     ramp_segment = sin.(2*pi*t).^2
     ramp = [ramp_segment; ones(length(signal) - length(ramp_segment)*2); reverse(ramp_segment)]
     return signal .* ramp
@@ -46,11 +48,11 @@ end
 """
     cosine_ramp!(signal, dur_ramp, fs)
 
-Applies a raised-cosine ramp to the input signal, where dur_ramp is the duration of each
+Applies a raised-cosine ramp to the input signal, where dur_ramp is the duration of each 
 samp segment in seconds
 """
 function cosine_ramp!(signal::AbstractVector{T}, dur_ramp::T, fs::T)::AbstractVector{T} where {T<:Real}
-    t = LinRange(0, 0.25, Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
+    t = LinRange(0, prevfloat(0.25), Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
     ramp_segment = sin.(2*pi*t).^2
     ramp = [ramp_segment; ones(length(signal) - length(ramp_segment)*2); reverse(ramp_segment)]
     signal .*= ramp
@@ -59,7 +61,7 @@ end
 
 
 """
-    dbspl(signal)::AbstractFloat
+    dbspl(signal)
 
 Calculates the dB SPL value of a signal (assuming that the units of the signal are in Pa)
 """
@@ -69,7 +71,9 @@ end
 
 
 """
-    LogRange(a, b, n)::Range
+    LogRange(a, b, n)
+
+Creates a vector with n elements spaced logarithmically from a to b
 """
 function LogRange(a::T, b::T, n::Int) where {T<:Real}
     10 .^ LinRange(log10(a), log10(b), n)
@@ -79,7 +83,8 @@ end
 """
     pure_tone(freq, phase, dur, fs)
 
-Synthesizes a pure tone with specified frequency, phase offset (in radians), duration, and sampling rate.
+Synthesizes a pure tone with specified frequency, phase offset (in radians), duration, and 
+sampling rate
 """
 function pure_tone(freq::T, phase::T, dur::T, fs::T)::AbstractVector{T} where {T<:Real}
     sin.(2*pi .* freq .* (0.0:(1/fs):prevfloat(dur)) .+ phase)
@@ -87,7 +92,7 @@ end
 
 
 """
-    rms(signal)::AbstractFloat
+    rms(signal)
 
 Calculates the room-mean-square (RMS) of a signal
 """
@@ -118,3 +123,4 @@ end
 
 
 end # module
+
