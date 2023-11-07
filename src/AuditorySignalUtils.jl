5b06bbc849f6538b1f2c71b44a86e090e5e516a7
Daniel Guest <guest121@umn.edu>
2023-11-07 11:07:04 -0500

    Started overhaul of ASU.jl
    Started overhaul, focusing on three things:
    - Removal of all type annotations from code (there is no need for them,
      except occasionally when clarifying roles of parameters or
      differentiating two methods)
    - Shorten docstrings to make them all one line, if possible
    - Addition of some utility functions that I want to use in future code
      (e.g., `samples`, `timeat`, `time`)

--- src/AuditorySignalUtils.jl
+++ src/AuditorySignalUtils.jl
@@ -3,44 +3,39 @@ module AuditorySignalUtils
 using Statistics
 using DSP: rms
 
-export amplify, amplify!, cosine_ramp, cosine_ramp!, dbspl, LogRange, pure_tone, 
-       scale_dbspl, scale_dbspl!, zero_pad
+export amplify, amplify!, 
+       cosine_ramp, cosine_ramp!, 
+       dbspl, 
+       LogRange, 
+       pure_tone, 
+       scale_dbspl, scale_dbspl!, 
+       zero_pad, zero_pad!
+
+samples(time, fs) = Int(round(time, fs))
+timeat(sample, fs) = sample*fs
+time(dur, fs) = (0.0:(1/fs):(dur-1/fs))
 
 """
     amplify(x, dB)
 
-Amplifies (or attenuates) a signal by an amount in dB
-
-Changes the power in signal `x` by decibel factor `dB`. When `dB` is positive, the power in
-the signal is increased (i.e., signal is amplified) and when `dB` is negative, the power in
-the signal is decreased (i.e, signal is attenuated). Output signal and input signal have an
-intensity ratio of 10^(`dB`/10) and an amplitude ratio of 10^(`dB`/20)
+Amplifies (or attenuates) signal by factor of 10^(`dB`/20)
 """
-function amplify(x::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:AbstractFloat}
-    x .* 10.0^(dB/20.0)
-end
-
-function amplify!(x::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:AbstractFloat}
-    x .*= 10.0^(dB/20.0)
-    return x
-end
+amplify!(x, dB) = x .*= 10.0^(dB/20.0)
+amplify(x, dB) = x .* 10.0^(dB/20.0)
 
 """
     cosine_ramp(x, dur_ramp, fs)
 
-Applies a raised-cosine ramp to an input signal
-
-Applies a raised-cosine ramp to the input signal `x` where the ramp has duration `dur_ramp`
-(s) and sampling rate `fs` (Hz)
+Applies raised-cosine ramp of dur `dur_ramp` (s) to input signal of sampling rate `fs`
 """
-function cosine_ramp(x::AbstractVector{T}, dur_ramp::T, fs::T)::AbstractVector{T} where {T<:Real}
+function cosine_ramp(x, dur_ramp, fs)
     t = LinRange(0, prevfloat(0.25), Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
     ramp_segment = sin.(2π .* t).^2
     ramp = [ramp_segment; ones(length(x) - length(ramp_segment)*2); reverse(ramp_segment)]
     return x .* ramp
 end
 
-function cosine_ramp!(x::AbstractVector{T}, dur_ramp::T, fs::T)::AbstractVector{T} where {T<:Real}
+function cosine_ramp!(x, dur_ramp, fs)
     t = LinRange(0, prevfloat(0.25), Int(floor(fs*dur_ramp)))  # f = 1 Hz, dur = 1/4 cycle, 0 -> 1
     ramp_segment = sin.(2π .* t).^2
     ramp = [ramp_segment; ones(length(x) - length(ramp_segment)*2); reverse(ramp_segment)]
@@ -51,13 +46,9 @@ end
 """
     dbspl(x)
 
-Calculates the dB SPL value of a signal
-
-Calculates the dB SPL of the whole signal `x` assuming that the signals unit's are Pa
+Calculate signal's RMS level in dB SPL re: 20μPa
 """
-function dbspl(x::AbstractVector{T})::T where {T<:Real}
-    20*log10(rms(x)/20e-6)
-end
+dbspl(x) = 20.0*log10(rms(x)/20e-6)
 
 """
     LogRange(a, b, n)
@@ -75,43 +66,18 @@ end
 """
     pure_tone(freq, phase, dur, fs)
 
-Synthesize a pure tone 
-
-Synthesize pure tone with with specified frequency `f` (Hz), phase offset `ϕ` (rads),
-duration `dur` (s), and sampling rate `fs` (Hz) 
+Synthesize pure tone; `freq` (Hz), phase `ϕ` (rads), `dur` (s), sample rate `fs` (Hz)
 """
-function pure_tone(freq::T, ϕ::T, dur::T, fs::T)::AbstractVector{T} where {T<:Real}
-    sin.(2π .* freq .* (0.0:(1/fs):(dur-1/fs)) .+ ϕ)
-end
+pure_tone(freq, ϕ, dur, fs) = sin.(2π .* freq .* time(dur, fs) .+ ϕ)
 
 """
-    pure_tone(freq, phase, dur, fs)
+    scale_dbspl(x, level)
 
-Synthesize a pure tone 
-
-Synthesize pure tone with with specified frequency `f` (Hz), phase offset `ϕ` (rads),
-duration `dur` (s), and sampling rate `fs` (Hz) 
+Adjusts signal's level to be `level` in dB SPL re: 20μPa
 """
-function pure_tone(freq::T, ϕ::M, dur::T, fs::T)::AbstractVector{T} where {T<:Real, M<:Irrational}
-    sin.(2π .* freq .* (0.0:(1/fs):(dur-1/fs)) .+ ϕ)
-end
-
-"""
-    scale_dbspl(x, dB)
-
-Adjusts a signal's level to be a certain level in dB SPL
-
-Modified signal `x` to have level `dB` in dB SPL 
-"""
-function scale_dbspl(x::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:Real}
-    curr_dB = dbspl(x)
-    delta_dB = dB - curr_dB
-    amplify(x, delta_dB)
-end
-
-function scale_dbspl!(x::AbstractVector{T}, dB::T)::AbstractVector{T} where {T<:Real}
-    amplify!(x, dB - dbspl(x))
-end
+scale_dbspl(x, level) = amplify(x, level - dbspl(x))
+scale_dbspl!(x, level) = amplify!(x, level - dbspl(x))
 
 end # module
+
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,15 +3,15 @@ using AuditorySignalUtils
 using DSP: rms
 
 # Declare various constants that hold across all tests
-const fs = 48e3
+const fs = 100e3
 
 # Test: amplify correctly adjusts power and amplitude ratios
 @testset "amplify" begin
     for dB in -50.0:5.0:50.0
         x_ref = pure_tone(1000.0, 0.0, 0.1, fs)
         x_amp = amplify(x_ref, dB)
-        amplitude_ratio = maximum(x_amp) / maximum(x_ref)
-        power_ratio = maximum(x_amp)^2 / maximum(x_ref)^2
+        amplitude_ratio = rms(x_amp) / rms(x_ref)
+        power_ratio = rms(x_amp)^2 / rms(x_ref)^2
         @test (
             (amplitude_ratio ≈ 10^(dB/20)) & 
             (power_ratio ≈ 10^(dB/10))
@@ -19,14 +19,14 @@ const fs = 48e3
     end
 end
 
-# Test: pure_tone() properly phase shifts a sine tone as function of input phase 
+# Test: `pure_tone` properly phase shifts a sine tone as function of input phase 
 @testset "Pure tone synthesis" begin
     for ϕ in LinRange(0.0, 2π, 20)
         @test pure_tone(5.0, ϕ, 0.01, fs)[1] ≈ sin(ϕ)
     end
 end
 
-# Test: db SPL returns the correct value for various reference sounds
+# Test: `dbspl` returns the correct value for various reference sounds
 @testset "dB SPL" begin
     @test dbspl(zeros(5000)) == -Inf
     @test dbspl(pure_tone(1000.0, 0.0, 1.0, fs)) ≈ 90.969 atol=0.01
